2e02496b0c00ef07312e3f07deda5a8952d6747b
Boi <78007404+boiss69@users.noreply.github.com>
2021-03-12 09:35:56 +0700

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -60,7 +60,7 @@ async def play(client: Client, message_: Message):
     try:
         is_playing = tgcalls.pytgcalls.is_playing(message_.chat.id)
     except:
-        is_playing = False
+        is_playing = True
 
     if is_playing:
         position = await sira.add(message_.chat.id, file_path)
